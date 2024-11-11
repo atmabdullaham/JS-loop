1466463be415bb860a25ddbd0593406d1bd3eaaf
atmabdullaham <atmabdullaham@gmail.com>
2024-11-11 19:27:15 +0600

    added the system of finding even number, range of numbers and sum of some specific numbers

---
+++ sum.js
@@ -0,0 +1,9 @@
+let num = 1;
+let sum = 0;
+while (num <= 10) {
+ console.log(num);
+ sum = sum + num;
+ console.log("This is from sum", sum);
+ num++;
+
+}
--- while.js
+++ while.js
@@ -1,6 +1,15 @@
-const number = 7;
+const number = 70;
 if (number < 10) {
  console.log("coto ekta num")
 }
 
 console.log("something new is cooking")
+
+// ________________While__Loop_________________
+
+let num = 0; //loop variable
+while (num < 5) {
+ // num = num + 1;
+ // num += 1;
+ num++;
+} 
