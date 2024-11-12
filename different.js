e4bceb22dc7a180cea435c408853b11e53f528cd
atmabdullaham <atmabdullaham@gmail.com>
2024-11-12 10:56:14 +0600

    practice the for loop for different practice and practice the break and continue

---
+++ different.js
@@ -0,0 +1,45 @@
+// odd numbers
+
+// for (let i = 0; i < 20; i++) {
+//  if (i % 2 !== 0) {
+//   console.log(i)
+//  }
+// }
+
+
+
+// for (let i = 1; i < 20; i += 2) {
+//  console.log(i);
+// }
+
+// give me the list of numbers between 1 to 30, divisible by 5;
+
+
+// for (let i = 1; i <= 30; i++) {
+//  if (i % 5 === 0) {
+//   console.log(i);
+//  }
+// }
+
+
+// for (let i = 1; i <= 30; i++) {
+//  if (i % 3 === 0 || i % 5 === 0) {
+//   console.log(i);
+//  }
+// }
+
+// for (let i = 1; i <= 30; i++) {
+//  if (i % 3 === 0 && i % 5 === 0) {
+//   console.log(i);
+//  }
+// }
+
+
+// give me the sum of numnbers form 1 to 20 that are divisible by 3
+let total = 0;
+for (let i = 1; i <= 20; i++) {
+ if (i % 3 === 0) {
+  total = total + i;
+ }
+}
+console.log(total)
